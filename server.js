2e78e3984688317ca238ae2cd2f75830c348804c
Kieran McCormick <octavian13@shaw.ca>
2019-01-26 14:12:49 -0800

    Add Pusher to Backend

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '700488',
+  key: '7e702683ef693aa93027',
+  secret: '031dc5769c6550b2d9c8',
+  cluster: 'us2',
+  encrypted: true
 });
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+pusher.trigger('my-channel', 'my-event', {
+  "message": "hello world"
+});
+
+var app = express();
+
+app.post('/comment', function(req, res){
+    console.log(req.body);
+    var newComment = {
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment
+    }
+    pusher.trigger('flash-comments', 'new_comment', newComment);
+    res.json({ created: true });
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Error Handler for 404 Pages
+app.use(function(req, res, next) {
+    var error404 = new Error('Route Not Found');
+    error404.status = 404;
+    next(error404);
+});
+
+module.exports = app;
+
+app.listen(9000, function(){
+  console.log('Example app listening on port 9000!')
+});
