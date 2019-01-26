51ba28cf3c8474841cc459d4a685ad99261d2cd8
Kieran McCormick <octavian13@shaw.ca>
2019-01-26 15:25:53 -0800

    Fixed bugs

--- server.js
+++ server.js
@@ -5,33 +5,32 @@ var bodyParser = require('body-parser');
 var Pusher = require('pusher');
 
 var pusher = new Pusher({
-  appId: '700488',
-  key: '7e702683ef693aa93027',
-  secret: '031dc5769c6550b2d9c8',
-  cluster: 'us2',
-  encrypted: true
-});
-
-pusher.trigger('my-channel', 'my-event', {
-  "message": "hello world"
+    appId: '700488',
+    key: '7e702683ef693aa93027',
+    secret: '031dc5769c6550b2d9c8',
+    cluster: 'us2',
+    useTLS: true
 });
 
 var app = express();
 
-app.post('/comment', function(req, res){
-    console.log(req.body);
-    var newComment = {
-      name: req.body.name,
-      email: req.body.email,
-      comment: req.body.comment
-    };
-    pusher.trigger('flash-comments', 'new_comment', newComment);
-    res.json({ created: true });
-  });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/comment', function(req, res){
+  console.log("fuck u");    
+  console.log(req.body);
+  console.log(req.body.comment);
+  
+  var newComment = {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment
+  };
+  pusher.trigger('flash-comments', 'new_comment', newComment);
+  res.json({ created: true });
+});
 
 // Error Handler for 404 Pages
 app.use(function(req, res, next) {
