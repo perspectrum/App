bb5cdc1c831f6bed8802575951205fdacb34b48a
Kieran McCormick <octavian13@shaw.ca>
2019-01-27 13:03:43 -0800

    Final push for now!

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ function isEmptyObject(obj) {
   
 
 module.exports = app;
-var port = 8080;
+var port = 80;
 app.listen(port, function(){
   console.log('Example app listening on port ' + port + "!");
 });
