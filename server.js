26b9c98f7e5199aea22abae275eb30246b76a92c
Unknown <carlsonc@sfu.ca>
2019-01-26 19:18:49 -0800

    get news

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var bodyParser = require('body-parser');
 
 var Pusher = require('pusher');
 
+var config = require('./config.json');
+var newsApiKey = config.newsapi.apikey;
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(newsApiKey);
+
+
 var pusher = new Pusher({
   appId: '700488',
   key: '7e702683ef693aa93027',
@@ -17,6 +24,22 @@ pusher.trigger('my-channel', 'my-event', {
 });
 
 var app = express();
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/perspectrum");
+
+var articleSchema = new mongoose.Schema({
+    source : {id: String, name: String},
+    author : String,
+    title : String,
+    description : String,
+    url : String,
+    urlToImage : String,
+    publishedAt : String,
+   });
+
+var Article = mongoose.model("Article", articleSchema);
 
 app.post('/comment', function(req, res){
     console.log(req.body);
@@ -33,12 +56,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/getarticle', function(req, res){
+    var dateNow = new Date();
+    var somedate= (dateNow.getMonth()+1).toString() + "-" + dateNow.getDate().toString() + "-" + dateNow.getFullYear().toString();
+
+    Article.find({publishedAt: somedate}, function(err,article){
+        if(err) throw err;
+        console.log(article);
+        res.json(article);
+    });
+    
+  });
 // Error Handler for 404 Pages
 app.use(function(req, res, next) {
     var error404 = new Error('Route Not Found');
     error404.status = 404;
     next(error404);
 });
+
+// Article.find({publishedAt: somedate}, function(err,article){
+//     if(err) throw err;
+//     console.log(article);
+// });
+
+newsapi.v2.topHeadlines({
+    sources: 'breitbart-news,bbc-news,the-verge',
+    language: 'en',
+  }).then(response => {
+    //console.log(response.articles[0]);
+    var dateNow = new Date();
+    var stringDate =  (dateNow.getMonth()+1).toString() + "-" + dateNow.getDate().toString() + "-" + dateNow.getFullYear().toString();
+    var myData = new Article({
+        source : {id: response.articles[0].source.id, name: response.articles[0].source.name},
+        author : response.articles[0].author,
+        title : response.articles[0].title,
+        description : response.articles[0].description,
+        url : response.articles[0].url,
+        urlToImage : response.articles[0].urlToImage,
+        publishedAt : stringDate});
+    myData.save(function(error) {
+        console.log("Your article has been saved!");
+    if (error) {
+        console.error(error);
+    }
+    });
+  });
 
 module.exports = app;
 
