1ecd0ec930105a289e8745204de2ca146b458bfb
Kieran McCormick <octavian13@shaw.ca>
2019-01-26 21:25:41 -0800

    Added auto gen article UI

--- public/app.js
+++ public/app.js
@@ -1,9 +1,25 @@
 // Using IIFE for Implementing Module Pattern to keep the Local Space for the JS Variables
 (function () {
+
+    var serverUrl = "/",
+        comments = [],
+        pusher = new Pusher('7e702683ef693aa93027', {
+            cluster: 'us2',
+            encrypted: true
+        }),
+        // Subscribing to the 'flash-comments' Channel
+        channel = pusher.subscribe('flash-comments'),
+        commentForm = document.getElementById('comment-form'),
+        commentsList = document.getElementById('comments-list'),
+        commentTemplate = document.getElementById('comment-template'),
+        articleTemplate = document.getElementById('article-template'),
+        article = document.getElementById('article');
+
+
     var getComments = new XMLHttpRequest();
     getComments.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            //console.log("!!!!!!!!!");
+        if (getComments.readyState == XMLHttpRequest.DONE) {
+            // console.log("!!!!!!!!!");
             var json = JSON.parse(getComments.responseText);
             for (var i in json) {
                 console.log(json[i]);
@@ -22,10 +38,29 @@
 
     var getArticle = new XMLHttpRequest();
     getArticle.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
+        if (getArticle.readyState == XMLHttpRequest.DONE) {
             //console.log("!!!!!!!!!");
-            var json = JSON.parse(getArticle.responseText);
+            var data = JSON.parse(getArticle.responseText);
+            json = data[0];
             console.log(json);
+            console.log(json.author);
+            console.log(json.title);
+            console.log(json.url);
+            console.log(json.description);
+
+            var newArticleTemplate = articleTemplate.innerHTML.replace('{{author}}', json.author);
+            newArticleTemplate = newArticleTemplate.replace('{{title}}', json.title);
+            newArticleTemplate = newArticleTemplate.replace('{{url}}', json.url);
+            newArticleTemplate = newArticleTemplate.replace('{{imageurl}}', json.urlToImage);
+            newArticleTemplate = newArticleTemplate.replace('{{description}}', json.description);
+
+
+            console.log(newArticleTemplate);
+
+            var newArticleNode = document.createElement('div');
+            newArticleNode.classList.add('comment');
+            newArticleNode.innerHTML = newArticleTemplate;
+            article.appendChild(newArticleNode);
             // for (var i in json) {
             //     console.log(json[i]);
             //     var newCommentHtml = commentTemplate.innerHTML.replace('{{name}}', json[i].name);
@@ -44,17 +79,7 @@
     // Enable pusher logging - don't include this in production
     Pusher.logToConsole = true;
 
-    var serverUrl = "/",
-        comments = [],
-        pusher = new Pusher('7e702683ef693aa93027', {
-            cluster: 'us2',
-            encrypted: true
-        }),
-        // Subscribing to the 'flash-comments' Channel
-        channel = pusher.subscribe('flash-comments'),
-        commentForm = document.getElementById('comment-form'),
-        commentsList = document.getElementById('comments-list'),
-        commentTemplate = document.getElementById('comment-template');
+
 
     // Binding to Pusher Event on our 'flash-comments' Channel
     channel.bind('new_comment', newCommentReceived);
